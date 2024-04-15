5cb0c6181001bfdb4a1163c6c12851895b3078e4
Konstantin Mokievets <k.mokievets@gmail.com>
2024-04-15 23:36:05 +0300

    step_5_1 + rolling

--- step_5_1_finished.py
+++ step_5_1_finished.py
@@ -13,33 +13,17 @@ fig, pic = plt.subplots(figsize=(18, 6))
 #Data set for the Picture:
 pict_01_bars = df.groupby('date_group').agg(visits=('visits', 'sum')).reset_index()
 
-#Adding rolling value:
-pict_01_bars['rolling'] = pict_01_bars[['visits']].rolling(30, 1).mean().round(0)
-print(pict_01_bars)
-
-
-#Picture creation (PYPLOT):
-barchart = plt.bar(pict_01_bars['date_group'],
-                   pict_01_bars['visits'],
-                   color='orange',
-                   label='visits count')
-
-pic.set(xlabel='Date',
-        ylabel='Quantity of visitors',
-        title='Picture 01 - Total Visits')
-       
-
-linechart = plt.plot(pict_01_bars['date_group'],
-                     pict_01_bars['rolling'],
-                     color='blue',
-                     linestyle='dashed',
-                     label='rolling average (30 days)')
-                     
+#Picture creation:
+sns.barplot(data=pict_01_bars,
+            x=pict_01_bars.date_group,
+            y=pict_01_bars.visits,
+            color='orange').set(xlabel='Date',
+                                ylabel='Quantity of visitors',
+                                title='Picture 01 - Total Visits')
 
 #Settin up grids and ticks:
 plt.grid(axis='both', ls=':', color='grey')
 plt.yticks(np.arange(300, 1600, 100))
-plt.xticks(pict_01_bars.date_group[::3])
 
 #Rotate xlabels to 90 deg:
 pic.tick_params(axis='x', labelsize=8, labelrotation=45, grid_alpha=0.33)
@@ -50,9 +34,12 @@ fig.tight_layout()
 
 #Setting up limits for x an y axes:
 pic.set_ylim(300, 1500)
-xmargin = pd.Timedelta('3D') 
-pic.set_xlim(pict_01_bars.date_group.min() - xmargin,
-             pict_01_bars.date_group.max() + xmargin)
+x, y = pic.get_xlim(), pic.get_ylim()
+xmargin = 0.01 * (x[1] - x[0])
+pic.set_xlim(x[0] - xmargin, x[1] + xmargin)
+
+#X-ticks - each 8th date instead of all set:
+pic.set_xticks(pic.get_xticks()[::3])
 
 #X-ticks - set alignment of rotated labels:
 plt.setp(pic.get_xticklabels(), ha='right')
@@ -60,7 +47,6 @@ plt.setp(pic.get_xticklabels(), ha='right')
 #Bar labels show with 90deg rotation, color, size..:
 pic.bar_label(pic.containers[0], rotation=90, fontsize=7, color='black')
 
-plt.legend(loc='upper center')
 plt.show()
 
 
